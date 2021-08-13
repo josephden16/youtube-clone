62d4fdf2317546c97136fe01ee845c94a1785f21
josephden16 <tegajoseph2002@gmail.com>
2021-08-13 15:38:27 +0100

    refactor sidebar

--- src/components/navigation/SideBar.tsx
+++ src/components/navigation/SideBar.tsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { Link, useLocation } from 'react-router-dom'
 import { faFire, faFolder, faHome, faCompactDisc } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { UserContext } from '../providers/AuthProvider';
-import { firestore } from '../../firebase';
+import Subscriptions from './sidebar/Subscriptions';
 
 
 const SideBar = () => {
@@ -31,55 +31,6 @@ const SideBar = () => {
       </ul>
       {user && <Subscriptions user={user} />}
     </nav>
-  )
-}
-
-
-const Subscriptions = ({ user }) => {
-  const [subscriptions, setSubscriptions] = useState(null);
-  useEffect(() => {
-    const fetchSubscriptions = async () => {
-      const userId = user.uid;
-      const subscriptionsRef = firestore.collection("users").doc(userId).collection("subscriptions");
-      try {
-        const snapshot = await subscriptionsRef.get();
-        let channels = [];
-        let size = snapshot.docs.length;
-        snapshot.docs.forEach(async (doc, index) => {
-          const channelRef = firestore.collection("channels").doc(doc.id);
-          const snapshot = await channelRef.get();
-          const data = { channelId: snapshot.id, ...snapshot.data() };
-          channels.push(data);
-          if (index === size - 1) {
-            setSubscriptions(channels);
-          }
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    fetchSubscriptions();
-  }, [user]);
-
-  if (!(subscriptions && user)) return null;
-
-  return (
-    <ul className={subscriptions ? "dark:text-lightGray m-0 mt-16 space-y-4 text-gray" : 'hidden'}>
-      {subscriptions.length > 0 && <h2 className="dark:text-white text-dark font-bold text-xl mb-4">Subscriptions</h2>}
-      {subscriptions && subscriptions.map((channel: any) => (
-        <li key={channel.channelId}>
-          <Link to={`/channel/${channel.channelId}`} className="flex space-x-3 hover:opacity-80">
-            <span>
-              <img className="rounded-circle" style={{ width: '30px', height: '30px' }} src={channel.channelPhotoURL} alt="channel" />
-            </span>
-            <span className="mt-1 text-sm">
-              {channel.channelName}
-            </span>
-          </Link>
-        </li>
-      ))}
-    </ul>
   )
 }
 
