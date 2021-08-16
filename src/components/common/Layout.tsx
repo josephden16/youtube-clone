7d2015cf54357961da6295ec2ec765c4c23ec360
josephden16 <tegajoseph2002@gmail.com>
2021-08-16 23:35:20 +0100

    add adjustments to site layout

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -15,8 +15,8 @@ const Layout = ({ children }) => {
     <>
       <div className="dark:bg-dark transition-colors duration-500 h-full md:pt-0 pl-2 pr-2 pb-20 lg:pr-4 lg:pl-4">
         <Header sidebar={true} handleMenu={handleSideBar} />
-        <div className="flex w-full pt-10 lg:pt-8 md:space-x-8 lg:space-x-11 xl:space-x-14">
-          <div className={navOpen ? 'openSidebar lg:mr-16' : 'hideSidebar lg:mr-0'}>
+        <div className="flex w-full pt-10 lg:pt-4 md:space-x-8 lg:space-x-11 xl:space-x-1">
+          <div className={navOpen ? 'openSidebar lg:mr-16 xl:w-56' : 'hideSidebar lg:mr-0'}>
             <SideBar />
           </div>
           <main className="w-full">{children}</main>
