948047f90735d86915c65ef08857b2178f46b311
Denedo <tegajoseph2002@gmail.com>
2022-06-23 06:43:14 +0100

    fix: update search implementation

--- src/api/requests/videos.ts
+++ src/api/requests/videos.ts
@@ -20,3 +20,33 @@ export const getHomePageVideos = async () => {
     throw error;
   }
 };
+
+export const searchVideos = async (searchQuery: string) => {
+  try {
+    const snapshot = await firestore.collection("videos").get();
+
+    const videos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+
+    console.log(videos);
+
+    if (videos && videos.length > 0) {
+      const filteredVideos = videos.filter(
+        (video: any) =>
+          video.title.toLowerCase().indexOf(searchQuery) > -1 ||
+          video.description.toLowerCase().indexOf(searchQuery) > -1 ||
+          video.channelName.toLowerCase().indexOf(searchQuery) > -1
+      );
+
+      console.log(filteredVideos);
+      return filteredVideos;
+    }
+    return [];
+  } catch (error) {
+    throw error;
+  }
+};
