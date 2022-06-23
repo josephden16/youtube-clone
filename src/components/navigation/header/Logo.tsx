fff2ee98fec758aa5ff62e3624a2b678da717564
Denedo <tegajoseph2002@gmail.com>
2022-06-23 06:30:40 +0100

    fix: refactor theme handling and profile popup

--- src/components/navigation/header/Logo.tsx
+++ src/components/navigation/header/Logo.tsx
@@ -1,18 +1,25 @@
-import logoLight from '../../../images/logo-light.svg';
-import logoDark from '../../../images/logo-dark.svg';
+import logoLight from "../../../images/logo-light.svg";
+import logoDark from "../../../images/logo-dark.svg";
+import { useTheme } from "../../providers/ThemeProvider";
 
+const Logo = () => {
+  const { theme } = useTheme();
 
-const Logo = ({ theme }) => {
-  let classList = document.documentElement.classList;
-
-  if (classList.contains("dark") || theme === "dark") {
-    return <img src={logoDark} className="mt-2 md:mt-3 lg:mt-3 w-30" alt="youtube" />
+  if (theme === "dark") {
+    return (
+      <img src={logoDark} className="mt-2 md:mt-3 lg:mt-3 w-30" alt="youtube" />
+    );
   }
 
-  if (!(classList.contains("dark")) || theme === "light") {
-    return <img src={logoLight} className="mt-2 md:mt-3 lg:mt-3 w-30" alt="youtube" />
+  if (theme === "light") {
+    return (
+      <img
+        src={logoLight}
+        className="mt-2 md:mt-3 lg:mt-3 w-30"
+        alt="youtube"
+      />
+    );
   }
-}
-
+};
 
 export default Logo;
--- src/components/navigation/header/Profile.tsx
+++ src/components/navigation/header/Profile.tsx
@@ -1,6 +1,16 @@
+import { Popover, Transition } from "@headlessui/react";
 import { BiLogIn } from "react-icons/bi";
+import { FaRegUser } from "react-icons/fa";
+import { GoSignOut } from "react-icons/go";
 
-const Profile = ({ user, handleModal, handleSignIn, loading }) => {
+const Profile = ({
+  user,
+  handleModal,
+  handleSignIn,
+  loading,
+  handleSignOut,
+  goToProfilePage,
+}) => {
   if (loading)
     return (
       <div
@@ -11,14 +21,49 @@ const Profile = ({ user, handleModal, handleSignIn, loading }) => {
 
   if (user) {
     return (
-      <button style={{ outline: "none" }} onClick={handleModal}>
-        <img
-          className="hover:opacity-70 rounded-circle mt-1 lg:mt-1 ml-2 cursor-pointer"
-          style={{ width: "30px", height: "30px" }}
-          src={user.photoURL}
-          alt="profile"
-        />
-      </button>
+      <Popover className="relative">
+        <Popover.Button style={{ outline: "none" }} onClick={handleModal}>
+          <img
+            className="hover:opacity-70 rounded-circle mt-1 lg:mt-1 ml-2 cursor-pointer"
+            style={{ width: "30px", height: "30px" }}
+            src={user.photoURL}
+            alt="profile"
+          />
+        </Popover.Button>
+        <Transition
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+        >
+          <Popover.Panel className="absolute -right-2  z-10 w-48 rounded-md py-2 text-dark shadow-sm dark:text-white lg:right-0">
+            <div
+              className={
+                "dark:bg-dark2 bg-lightGray rounded-md p-2 space-y-3 absolute right-4 mt-3 lg:right-8 z-40 w-52 opacity-100 transition-opacity"
+              }
+            >
+              {user && (
+                <button
+                  onClick={goToProfilePage}
+                  className="rounded-md space-x-2 hover:opacity-75 w-full font-bold flex flex-row items-center text-sm justify-center"
+                >
+                  <span>Your Channel</span>
+                  <FaRegUser size="1.2em" />
+                </button>
+              )}
+              <button
+                onClick={handleSignOut}
+                className="rounded-md space-x-2 hover:opacity-70 w-full font-bold flex flex-row items-center text-sm justify-center"
+              >
+                <span>Sign out</span>
+                <GoSignOut className="relative" style={{ top: "2px" }} />
+              </button>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
     );
   }
   return (
@@ -27,7 +72,7 @@ const Profile = ({ user, handleModal, handleSignIn, loading }) => {
       onClick={handleSignIn}
       className="focus:text-opacity-60 flex items-center outline-none rounded-md border-black dark:border-lightGray"
     >
-        <BiLogIn style={{top: '4px'}} className="relative left-1 text-2xl" />
+      <BiLogIn style={{ top: "4px" }} className="relative left-1 text-2xl" />
     </button>
   );
 };
