9b2682c127c8c315e8f10bb955620a9fa8b7ad70
Denedo <tegajoseph2002@gmail.com>
2022-06-23 07:08:32 +0100

    fix: handle video not found error properly

--- src/pages/Watch/index.tsx
+++ src/pages/Watch/index.tsx
@@ -3,7 +3,11 @@ import { useLocation } from "react-router-dom";
 import VideoPlayer from "../../components/watch/VideoPlayer";
 import RelatedVideos from "../../components/watch/RelatedVideos";
 import Layout from "../../components/common/Layout";
-import { useRelatedVideos, useVideo, watchVideoQueryKey } from "../../api/hooks/watch";
+import {
+  useRelatedVideos,
+  useVideo,
+  watchVideoQueryKey,
+} from "../../api/hooks/watch";
 import "./watch.css";
 
 const useQuery = () => {
@@ -13,33 +17,64 @@ const useQuery = () => {
 const Watch = () => {
   let query = useQuery();
   let videoId = query.get("v");
-  const [checked, setCheck] = useState(false);
   const { videoLoading, data } = useVideo(videoId);
   const { relatedVideosLoading, relatedVideos } = useRelatedVideos(videoId);
-  const handleSwitch = () => {
-    setCheck(!checked);
-  };
 
   return (
     <Layout>
       <main className="layout mt-3 lg:-ml-1 lg:mt-10 w-full lg:space-x-10">
-        <VideoPlayer
-          autoplay={checked}
-          nextVideoId={relatedVideos ? relatedVideos[0]?.id : ""}
-          channelData={data?.channelData}
+        <Video
           videoId={videoId}
-          data={data?.videoData}
-          loading={videoLoading}
-          queryKey={watchVideoQueryKey}
-        />
-        <RelatedVideos
+          data={data}
           relatedVideos={relatedVideos}
-          checked={checked}
-          handleSwitch={handleSwitch}
-          loading={relatedVideosLoading}
+          relatedVideosLoading={relatedVideosLoading}
+          videoLoading={videoLoading}
         />
       </main>
     </Layout>
+  );
+};
+
+const Video = ({
+  data,
+  relatedVideos,
+  videoId,
+  relatedVideosLoading,
+  videoLoading,
+}) => {
+  const [checked, setCheck] = useState(false);
+  const handleSwitch = () => {
+    setCheck(!checked);
+  };
+
+  const loading = relatedVideosLoading || videoLoading;
+
+  if (!loading && !data) {
+    return (
+      <div className="flex w-full h-full items-center justify-center my-10">
+        <div>No video found</div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <VideoPlayer
+        autoplay={checked}
+        nextVideoId={relatedVideos ? relatedVideos[0]?.id : ""}
+        channelData={data?.channelData}
+        videoId={videoId}
+        data={data?.videoData}
+        loading={loading}
+        queryKey={watchVideoQueryKey}
+      />
+      <RelatedVideos
+        relatedVideos={relatedVideos}
+        checked={checked}
+        handleSwitch={handleSwitch}
+        loading={loading}
+      />
+    </>
   );
 };
 
